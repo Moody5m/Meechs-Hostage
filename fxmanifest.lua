b4e1cac3ec3aa34896a6c4eae9d3e9ed47488b37
Meech <111657985+Swivfty@users.noreply.github.com>
2024-05-13 18:57:53 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,3 +5,14 @@ this_is_a_map "yes"
 
 author 'Meech'
 description 'Meechs Take Hostage'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_script {
+    'cl_takehostage.lua'
+}
+server_script {
+    'sv_takehostage.lua'
+}
